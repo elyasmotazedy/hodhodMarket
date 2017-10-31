b56ac2c7534d856cd88f4650dcbac59d4af5a89a
Shahriyar <shahryar.tarnasi@gmail.com>
2017-10-31 21:58:13 +0330

    sa

--- includes/sections/admin_header.php
+++ includes/sections/admin_header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- CSS -->
-	<link rel="stylesheet" href="/hodhodMarket/css/bootstrap.css"; >
+	<link rel="stylesheet" href="/hodhodMarket/css/bootstrap.css" >
 	<link rel="stylesheet" type="text/css" href="/hodhodMarket/css/admin_style.css">
 
 	<!-- jQuery -->
